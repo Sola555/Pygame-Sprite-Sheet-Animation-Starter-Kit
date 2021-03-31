2144196a7a2b6f80ec07eb717ac6ac8da5b5649a
Sola555 <sola.lai.0000@gmail.com>
2021-03-31 14:03:24 -0700

    added sprite sheet part 1

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import pygame
-print("Hello world")
+
+#sprite sheet class
+class Spritesheet:
+    def __init__(self, img_path):
+        self.sprite_sheet = pygame.image.load(img_path)
+    def get_image(self, x, y, width, height):
+        image = pygame.Surface([width, height])
+        image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
