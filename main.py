94da5e2293c93268185ca7f6e452a1e76610ece6
Tan <atpulley@tutanota.com>
2021-04-02 11:53:42 -0700

    edits

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import pygame, sys
 from SpriteLoader import *
 from pygame.locals import *
 from Cat import *
+
+
 pygame.init()
 screen_info = pygame.display.Info()
 size_x = 800
 size_y = 600
 screen = pygame.display.set_mode((size_x, size_y))
-
+screen_info = pygame.display.Info()
 size = (width, height) = (screen_info.current_w, screen_info.current_h)
+screen = pygame.display.set_mode(size)
 
 clock = pygame.time.Clock()
 color = (230, 220, 190)
@@ -22,39 +25,31 @@ cat_images = []
 #cat images:
 def get_images():
     cat_sheet = Spritesheet('runningcat.png')
-    for  i in range(4):
+    for i in range(4):
         for j in range(2):
             cat_images.append(cat_sheet.get_image(j * 512, i * 256, 512, 256))
             cat_images[-1] = pygame.transform.smoothscale(cat_images[-1], (180, 90))
-
-
-
-
-
 
 #main funcions
 def main():
     #get images
     get_images()
     cat = Cat((-90, random.randint(50, height-50)), cat_images)
-    cat_image = cat_images[0]
-    cat_rect = cat_image.get_rect()
+    # cat_image = cat_images[0]
+    # cat_rect = cat_image.get_rect()
 
     
 
     #exit screen
     while True:
-
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == QUIT:
-                sys.exit
-
+                sys.exit()
         #frame rate
-        cat.update('self')
-        cat.draw(screen)
+        cat.update()
         screen.fill(color)
-        
+        cat.draw(screen)
         pygame.display.flip()
 
     
